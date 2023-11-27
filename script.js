6732f9f8ef5aac5a68a37a896aa15e1ba9633d68
catupik <gnedyshevaeg@gmail.com>
2023-11-27 13:22:56 +0100

    corrections

--- script.js
+++ script.js
@@ -12,12 +12,50 @@ const image = document.querySelector('#image');
 
 let timerId;
 
-const timerObject = {
+
+
+const timerObject = JSON.parse(localStorage.getItem('timerObject')) || {
     state: "stopped",
     initialTime: 1500,
     secondsLeft: 1500,
-    
+    lastUpdate: Date.now(),
+};
+
+function ajustTimer() {
+    const currentTime = Date.now();
+    const elapsed = Math.floor((currentTime -  timerObject.lastUpdate)/ 1000);
+
+    if (timerObject.state === 'run'){
+        
+        timerObject.secondsLeft = Math.max(timerObject.secondsLeft - elapsed, 0);
+
+        if(timerObject.secondsLeft === 0) {
+            stopTimer();
+        } else {
+            
+            startTimer();
+        }
+    }
 }
+
+
+function updateTimer(){
+    let minutes = Math.floor(timerObject.secondsLeft/60) ;
+    let seconds = timerObject.secondsLeft%60;
+    if (seconds<10){
+        seconds = '0' + seconds;
+    }
+    countdown.textContent = `${minutes} : ${seconds}`;   
+}
+
+function saveTimerState(){
+    timerObject.lastUpdate = Date.now()
+    localStorage.setItem('timerObject', JSON.stringify(timerObject));
+}
+
+ajustTimer();
+updateTimer();
+
 
 short.addEventListener('click', function(){
     clearInterval(timerId);
@@ -26,6 +64,7 @@ short.addEventListener('click', function(){
     timerObject.secondsLeft = timerObject.initialTime;
     updateTimer();
     image.setAttribute('src', 'https://cdn.glitch.global/9af89f45-29c6-43a3-a0cb-e450d9fbcc77/tea-tomato-cartoon-1.svg?v=1669576628580');
+    saveTimerState();
 })
 
 long.addEventListener('click', function(){
@@ -35,6 +74,7 @@ long.addEventListener('click', function(){
     timerObject.secondsLeft = timerObject.initialTime;
     updateTimer();
     image.setAttribute('src', 'https://cdn.glitch.global/9af89f45-29c6-43a3-a0cb-e450d9fbcc77/chill-Tomato-cartoon-1.svg?v=1669576621824');
+    saveTimerState();
 })
 pomodoro.addEventListener('click', function(){
     clearInterval(timerId);
@@ -43,44 +83,41 @@ pomodoro.addEventListener('click', function(){
     timerObject.secondsLeft = timerObject.initialTime;
     updateTimer();
     image.setAttribute('src', 'https://cdn.glitch.global/9af89f45-29c6-43a3-a0cb-e450d9fbcc77/tomato-cartoon-1.svg?v=1669576632248');
+    saveTimerState();
 })
 
-function updateTimer(){
-    let minutes = Math.floor(timerObject.secondsLeft/60) ;
-    let seconds = timerObject.secondsLeft%60;
-    if (seconds<10){
-        seconds = '0' + seconds;
-    }
-    countdown.textContent = `${minutes} : ${seconds}`;
-    
-}
-updateTimer();
+function startTimer(){
 
-start.addEventListener('click', function func(){
-    if (timerObject.state === 'run') return;
+    clearInterval(timerId);
     timerObject.state = 'run';
-    
+    saveTimerState();
     timerId = setInterval(function(){
-    updateTimer();
-    timerObject.secondsLeft--;
-    if (timerObject.secondsLeft <= 0) {
-        stopTimer();
-        timerObject.secondsLeft = 0;
-    }   
-    
-        function stopTimer(){
-        clearInterval(timerId);
-    }
+        updateTimer();
+            timerObject.secondsLeft--;
+
+            if (timerObject.secondsLeft <= 0) {
+                stopTimer();
+                timerObject.secondsLeft = 0;
+    }       
+    }, 1000);
+}
 
 
-    }, 1000)
-})
+start.addEventListener('click', startTimer);
+
     
+function stopTimer(){
+    clearInterval(timerId);
+    timerObject.state = 'stopped';
+    timerObject.lastUpdate = Dtae.now();
+    saveTimerState();
+}
     
 
 pause.addEventListener('click', function(){
     timerObject.state = 'stopped';
     clearInterval(timerId);
+    saveTimerState();
     
 })
 
@@ -90,7 +127,7 @@ reset.addEventListener('click', function(){
     timerObject.state = 'stopped';
     updateTimer();
     clearInterval(timerId);
-    
+    saveTimerState();
 })   
     
     
